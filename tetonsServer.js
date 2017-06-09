ea297a4a9a03d1930f31d50d74e08558cc5de89d
foxinatardis <foxinatardis@gmail.com>
2017-06-09 09:30:06 -0600

    initial commit

---
+++ tetonsServer.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var compression = require('compression');
+var PORT = 9004;
+
+var http = express();
+http.use(compression());
+
+http.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/html/index.html');
+});
+
+http.use(express.static(__dirname + '/public'));
+
+http.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('You broke it!');
+});
+
+http.listen(PORT);
+console.log('Tetons server listening on port: ' + PORT);
