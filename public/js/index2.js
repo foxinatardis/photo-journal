1de459b133ece866f70f959759d1b3f603bf36ba
foxinatardis <foxinatardis@gmail.com>
2017-06-12 11:12:46 -0600

    refactored index2.js to improve modularity of view creation

--- public/js/index2.js
+++ public/js/index2.js
@@ -55,63 +55,82 @@ function placeScrollStops() {
 
 function createViews() {
 	for (var i = 0; i < viewOptions.length; i++) {
-		var view;
-		var viewWrapper = $(document.createElement('div'));
-		var viewBackground = $(document.createElement('div'));
-		var overlay = $(document.createElement('div'));
-		var overlayText = $(document.createElement('p'));
-		viewWrapper.css({
-			'z-index': viewOptions.length - i,
-			'position': 'fixed',
-			'top': 0,
-			'width': '100%',
-			'display': 'block',
-			'opacity': 1
-		});
-		if(viewOptions[i].backgroundStyles) viewBackground.css(viewOptions[i].backgroundStyles);
-		viewBackground.css({
-			'position': 'relative',
-			'height': '100vh',
-			'background': viewOptions[i].backgroundUrl,
-			'background-position': 'center',
-			'background-repeat': 'no-repeat',
-			'background-size': 'cover',
-		});
-		overlay.css({
-			'background-color': '#eee',
-			'border-radius': '5px',
-			'padding': '5vh 5vw',
-			'max-width': '50vw',
-			'left': '5vw',
-			'box-shadow': '0 0 10px #111'
-		});
-		if(viewOptions[i].overlayStyles) overlay.css(viewOptions[i].overlayStyles);
-		overlay.css({
-			'max-height': '90vh',
-			'position': 'absolute',
-			'top': '100vh'
-		});
-		overlayText.text(viewOptions[i].overlayText);
-		if(viewOptions[i].overlayTextStyles) overlayText.css(viewOptions[i].overlayTextStyles);
-		overlay.append(overlayText);
-		viewBackground.append(overlay);
-		viewWrapper.append(viewBackground);
-		$('body').append(viewWrapper);
-		view = {
-			viewWrapper: viewWrapper,
-			viewBackground: viewBackground,
-			overlay: overlay,
-			overlayText: overlayText,
+		var view = {
+			viewWrapper: createViewWrapper(viewOptions[i], viewOptions.length - i),
+			viewBackground: createViewBackground(viewOptions[i]),
+			overlay: createOverlay(viewOptions[i]),
+			overlayText: createOverlayText(viewOptions[i]),
 			scrollStop: $('.scrollStop-' + (i + 1))
 		};
+		view.overlay.append(view.overlayText);
+		view.viewBackground.append(view.overlay);
+		view.viewWrapper.append(view.viewBackground);
+		$('body').append(view.viewWrapper);
 		views.push(view);
 	}
+}
+
+function createViewWrapper(viewOptions, index) {
+	var viewWrapper = $(document.createElement('div'));
+	viewWrapper.css({
+		'z-index': index,
+		'position': 'fixed',
+		'top': 0,
+		'width': '100%',
+		'display': 'block',
+		'opacity': 1
+	});
+	return viewWrapper;
+}
+
+function createViewBackground(viewOptions) {
+	var viewBackground = $(document.createElement('div'));
+	if(viewOptions.backgroundStyles) viewBackground.css(viewOptions.backgroundStyles);
+	viewBackground.css({
+		'position': 'relative',
+		'height': '100vh',
+		'background': viewOptions.backgroundUrl,
+		'background-position': 'center',
+		'background-repeat': 'no-repeat',
+		'background-size': '100%',
+	});
+	return viewBackground;
+}
+
+function createOverlay(viewOptions) {
+	var overlay = $(document.createElement('div'));
+	overlay.css({
+		'background-color': '#eee',
+		'border-radius': '5px',
+		'padding': '5vh 5vw',
+		'max-width': '50vw',
+		'left': '5vw',
+		'box-shadow': '0 0 10px #111',
+		'opacity': '0.8'
+	});
+	if(viewOptions.overlayStyles) overlay.css(viewOptions.overlayStyles);
+	overlay.css({
+		'max-height': '90vh',
+		'position': 'absolute',
+		'top': '100vh'
+	});
+	return overlay;
+}
+
+function createOverlayText(viewOptions) {
+	var overlayText = $(document.createElement('p'));
+	overlayText.text(viewOptions.overlayText);
+	if(viewOptions.overlayTextStyles) overlayText.css(viewOptions.overlayTextStyles);
+	return overlayText;
 }
 
 function updateLoop() {
 	var scrollStopOffsetTop = views[currentViewIndex].scrollStop.offset().top;
 	var scrollStopHeight = views[currentViewIndex].scrollStop.height();
 	var overlay = views[currentViewIndex].overlay;
+
+	var viewBackground = views[currentViewIndex].viewBackground;
+
 	var viewWrapper = views[currentViewIndex].viewWrapper;
 	var overlayHeight = overlay.height();
 	var windowHeight = $(window).height();
@@ -121,6 +140,8 @@ function updateLoop() {
 	var newOverlayTopOffset = heightToScrollOverlay - (windowHeight * (1 - scrollPercentageFromTop) * 1.3);
 
 	overlay.css('top', newOverlayTopOffset);
+
+	// if(scrollPercentageFromTop > 0) viewBackground.css('transform', 'scale(' + (1 + ((1 - scrollPercentageFromTop))) + ')');
 
 	if(scrollPercentageFromTop > 0.9 && currentViewIndex > 0) {
 
